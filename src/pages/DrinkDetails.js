aa4dc88f4c84700c7f56245ea7a208461407c77b
Wellington <wellington.gts21@gmail.com>
2024-09-06 09:07:32 -0300

    Hoje

--- src/pages/DrinkDetails.js
+++ src/pages/DrinkDetails.js
@@ -1,14 +1,91 @@
-import { View, Text } from "react-native";
-import { styles } from "../styles/StyleSheet";
+import { View, Text, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { useRoute } from '@react-navigation/native';
+import axios from 'axios';
 
 export default function Drink() {
+
+  IP_URL = "10.144.170.34";
+
+  const route = useRoute();
+  const { id, image, data } = route.params;  // Recebe o ID do produto passado na navegação
+  const [drink, setDrink] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get(`http://${IP_URL}:3000/produtos/${id}`)
+    .then(response => {
+        setDrink(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error(error);
+        setLoading(false);
+      });
+  }, [id]);
+
+  if (loading) {
+    return <ActivityIndicator size="large" color="#0000ff" />;
+  }
+
+  if (!drink) {
+    return <Text>Erro ao carregar o produto</Text>;
+  }
+
   const navigation = useNavigation();
   return (
-    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1, }}>
-			<Text style={{ textAlign: 'justify' }}> 
-				Hey, that is drink screen!!
-      </Text>
+    <ScrollView style={styles.ScrollView}>
+    <View style={styles.container}>
+     
+      <Image source={image} style={styles.image} />
+      <Text style={styles.name}>{drink.name}</Text>
+      <Text style={styles.description}>{drink.description}</Text>
+      <Text style={styles.ingredientsTitle}>Ingredientes:</Text>
+      <Text style={styles.ingredients}>{drink.recipe}</Text>
+      <Text style={styles.howToMakeTitle}>Modo de Preparo:</Text>
+      <Text style={styles.howToMake}>{drink.comofazer}</Text>      
     </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  image: {
+    width: '100%',
+    height: 500,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  name: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  description: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  ingredientsTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  ingredients: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  howToMakeTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  howToMake: {
+    fontSize: 16,
+  },
+  ScrollView: {
+    flex: 1
+  }
+});
--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -9,17 +9,17 @@ import PressComponent from '../components/PressableComponent';
 import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
 
 const data = [
-  { image: require('../assets/images/Drinks/Caipirinha.png'), characteristic: 'Caipirinha', icon: require('../assets/images/HeartNaked.png') },
-  { image: require('../assets/images/Drinks/MoscowMule.png'), characteristic: 'Moscow Mule', icon: require('../assets/images/HeartNaked.png') },
-  { image: require('../assets/images/Drinks/Sangria.png'), characteristic: 'Sangria', icon: require('../assets/images/HeartNaked.png') },
-  { image: require('../assets/images/Drinks/Margarita.png'), characteristic: 'Margarita', icon: require('../assets/images/HeartNaked.png') },
-  { image: require('../assets/images/Drinks/VirginOnTheBeach.png'), characteristic: 'Virgin On The Beach', icon: require('../assets/images/HeartNaked.png') },
-  { image: require('../assets/images/Drinks/Mojito.png'), characteristic: 'Mojito', icon: require('../assets/images/HeartNaked.png') },
-  { image: require('../assets/images/Drinks/PinaColada.png'), characteristic: 'Pina Colada', icon: require('../assets/images/HeartNaked.png') },
-  { image: require('../assets/images/Drinks/BlueHawaiian.png'), characteristic: 'Blue Hawaiian', icon: require('../assets/images/HeartNaked.png') },
-  { image: require('../assets/images/Drinks/Flower.png'), characteristic: 'Flower', icon: require('../assets/images/HeartNaked.png') },
-  { image: require('../assets/images/Drinks/WattermelonSlushie.png'), characteristic: 'Wattermelon Slushie', icon: require('../assets/images/HeartNaked.png') },
-  { image: require('../assets/images/Drinks/Frame.png') }
+  { id: 1, image: require('../assets/images/Drinks/Caipirinha.png'), characteristic: 'Caipirinha', icon: require('../assets/images/HeartNaked.png') },
+  { id: 2, image: require('../assets/images/Drinks/MoscowMule.png'), characteristic: 'Moscow Mule', icon: require('../assets/images/HeartNaked.png') },
+  { id: 3, image: require('../assets/images/Drinks/Sangria.png'), characteristic: 'Sangria', icon: require('../assets/images/HeartNaked.png') },
+  { id: 4,image: require('../assets/images/Drinks/Margarita.png'), characteristic: 'Margarita', icon: require('../assets/images/HeartNaked.png') },
+  { id: 5,image: require('../assets/images/Drinks/VirginOnTheBeach.png'), characteristic: 'Virgin On The Beach', icon: require('../assets/images/HeartNaked.png') },
+  { id: 6,image: require('../assets/images/Drinks/Mojito.png'), characteristic: 'Mojito', icon: require('../assets/images/HeartNaked.png') },
+  { id: 7,image: require('../assets/images/Drinks/PinaColada.png'), characteristic: 'Pina Colada', icon: require('../assets/images/HeartNaked.png') },
+  { id: 8,image: require('../assets/images/Drinks/BlueHawaiian.png'), characteristic: 'Blue Hawaiian', icon: require('../assets/images/HeartNaked.png') },
+  { id: 9,image: require('../assets/images/Drinks/Flower.png'), characteristic: 'Flower', icon: require('../assets/images/HeartNaked.png') },
+  { id: 10,image: require('../assets/images/Drinks/WattermelonSlushie.png'), characteristic: 'Wattermelon Slushie', icon: require('../assets/images/HeartNaked.png') },
+  { id: 11,image: require('../assets/images/Drinks/Frame.png') }
 ];
 
 export default function Drinks() {
